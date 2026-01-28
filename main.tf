8e9bc02b70efb0e37435257362db9eee9e0d6a48
Nikita Gour <145796723+nikki152@users.noreply.github.com>
2026-01-28 16:13:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,4 +19,8 @@ resource "google_compute_instance" "linux-vm" {
     network = "default"
     access_config {}
   }
+
+metadata = {
+  ssh-keys = "jenkins:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTZ/FeJPNDLo5xqsBm+Fvnr3+W6JLU4Ykl4ldgApk0wFmFZeLH+u3czRZICs7P6QM68EDIWSJaaXo0M4bwooEAoGBvw2XD6ejlag58zEcRd+0KmMNMccgWMQoam+GdMmPk/VVEcaOr8VieL6EYNgeydsarFTCc718GfBjbZamDzWJAEvlaR8B46uXxuzeRKyrSNNkNX7fbIx6caWsSlMK/7UymTuc27TC0ubk4m3fObNmxocN29me/BL6/A7cNDELwvCjEbhuA7IzLTjmXwHZ+YEksTTbp//Oh/r66YeXYqe1ZKdUMDGGt/77bw8k7/JR6x2enH3KyghSkeRZPRLSv jenkins"
+ }
 }
